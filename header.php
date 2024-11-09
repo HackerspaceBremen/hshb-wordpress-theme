575b389578041379517e04fc03c4414a9a91ef58
Christian Friedrich Coors <me@ccoors.de>
2024-11-09 19:09:45 +0100

    fix: x link

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
                     <div id="shadow_r"></div>
                     <div id="socials">
                         <div id="fb"><a href="https://www.facebook.com/pages/Hackerspace-Bremen/217887601561428/" target="_blank"></a></div><!--
-                        --><div id="tw"><a href="https://twitter.com/hspacehb" target="_blank"></a></div><!--
+                        --><div id="tw"><a href="https://x.com/hspacehb" target="_blank"></a></div><!--
                         --><div id="ig"><a href="https://www.instagram.com/hackerspace_bremen/" target="_blank"></a></div><!--
                         --><div id="yt"><a href="https://www.youtube.com/channel/UC8xYE7eRekFjhs9ysyoe6gQ" target="_blank"></a></div><!--
                         --><div id="git"><a href="https://github.com/HackerspaceBremen" target="_blank"></a></div><!--
