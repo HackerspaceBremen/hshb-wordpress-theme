ce6972cc9ec83aa20ba1e9adecb0bc33a49cef26
Christian Friedrich Coors <me@ccoors.de>
2024-11-09 19:01:26 +0100

    fix: 404

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@ $custom_404_page = get_page_by_title('404');
 
 // Überprüfen, ob die Seite gefunden wurde und deren Inhalt anzeigen
 if ($custom_404_page) {
-    echo '<div id='content-wrapper'><div class='push-top'></div><div id='content' class='full'>';
+    echo '<div id="content-wrapper"><div class="push-top"></div><div id="content" class="full">';
     echo '<h1>' . esc_html($custom_404_page->post_title) . '</h1>';
     echo apply_filters('the_content', $custom_404_page->post_content);
     echo '</div>';
@@ -14,7 +14,7 @@ if ($custom_404_page) {
     echo '<div class="error-404">';
     echo '<h1>Seite nicht gefunden</h1>';
     echo '<p>Entschuldigung, die Seite konnte nicht geladen werden.</p>';
-    echo '</div></div><div class='push'></div><div class='clear'></div></div><!-- content-wrapper -->';
+    echo '</div></div><div class="push"></div><div class="clear"></div></div><!-- content-wrapper -->';
 }
 
 get_footer();
